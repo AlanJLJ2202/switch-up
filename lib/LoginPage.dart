4dd1ff532e964fad07067449adbf37f199cc1a97
AlanJLJ2202 <lrs19110248@purisima.tecnm.mx>
2022-04-11 22:53:09 -0500

    LoginPage

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,112 +1,219 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:switch_up/TabPage.dart';
 
 class LoginPage extends StatefulWidget{
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
-Widget buildEmail(){
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-      Text(
-        'Email',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 16,
-          fontWeight: FontWeight.bold
+
+
+class _LoginPageState extends State<LoginPage>{
+
+  bool isRememberMe = false;
+
+  Widget buildEmail(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Email',
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 26,
+              fontWeight: FontWeight.bold
+          ),
+        ),
+        SizedBox(height: 10),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 6,
+                    offset: Offset(0,2)
+                )
+              ]
+          ),
+          height: 60,
+          child: TextField(
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+                color: Colors.black87
+            ),
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(top:14),
+                prefixIcon: Icon(
+                  Icons.email,
+                  color: Color(0xff0E0E0E),
+                ),
+                hintText: 'Email',
+                hintStyle: TextStyle(
+                    color: Colors.black38
+                )
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget buildPassword(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Password',
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 26,
+              fontWeight: FontWeight.bold
+          ),
+        ),
+        SizedBox(height: 10),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 6,
+                    offset: Offset(0,2)
+                )
+              ]
+          ),
+          height: 60,
+          child: TextField(
+            obscureText: true,
+            style: TextStyle(
+                color: Colors.black87
+            ),
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(top:14),
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: Color(0xff0E0E0E),
+                ),
+                hintText: 'Password',
+                hintStyle: TextStyle(
+                    color: Colors.black38
+                )
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget buildForgotPassBtn(){
+    return Container(
+      alignment: Alignment.centerRight,
+      child: FlatButton(
+        onPressed: () => print("Forgot Pass Pressed"),
+        padding: EdgeInsets.only(right: 0),
+        child: Text(
+          '¿Ha olvidado su contraseña?',
+          style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold
+
+          ),
         ),
       ),
-      SizedBox(height: 10),
-      Container(
-        alignment: Alignment.centerLeft,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 6,
-              offset: Offset(0,2)
+    );
+  }
+
+  Widget buildRememberCb(){
+    return Container(
+      height: 20,
+      child: Row(
+        children: <Widget>[
+          Theme(
+            data: ThemeData(unselectedWidgetColor: Colors.white),
+            child: Checkbox(
+              value: isRememberMe,
+              checkColor: Colors.green,
+              activeColor: Colors.white,
+              onChanged: (value) {
+                setState(() {
+                  isRememberMe = value!;
+                });
+              },
+            ),
+          ),
+          Text(
+            'Recordar',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildLoginBtn(){
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 45),
+      width: double.infinity,
+      child: RaisedButton(
+        elevation: 20,
+        onPressed: () => print('Login Pressed'),
+        padding: EdgeInsets.all(15),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(55)
+        ),
+        color: Colors.white,
+        child: Text(
+          'Ingresar',
+          style: TextStyle(
+            color: Color(0xcc000000),
+            fontSize: 18,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildSignUpBtn(){
+    return GestureDetector(
+      onTap: () => print ("Sign up Pressed"),
+      child: RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: 'No tiene una cuenta?  ',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.w500
+              )
+            ),
+            TextSpan(
+              text: 'Registrarse',
+              
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 18,
+                fontWeight: FontWeight.bold
+              )
             )
           ]
         ),
-        height: 60,
-        child: TextField(
-          keyboardType: TextInputType.emailAddress,
-          style: TextStyle(
-            color: Colors.black87
-          ),
-          decoration: InputDecoration(
-            border: InputBorder.none,
-            contentPadding: EdgeInsets.only(top:14),
-            prefixIcon: Icon(
-              Icons.email,
-              color: Color(0xff0E0E0E),
-            ),
-            hintText: 'Email',
-            hintStyle: TextStyle(
-              color: Colors.black38
-            )
-          ),
-        ),
-      )
-    ],
-  );
-}
-
-Widget buildPassword(){
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-      Text(
-        'Password',
-        style: TextStyle(
-            color: Colors.white,
-            fontSize: 16,
-            fontWeight: FontWeight.bold
-        ),
       ),
-      SizedBox(height: 10),
-      Container(
-        alignment: Alignment.centerLeft,
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 6,
-                  offset: Offset(0,2)
-              )
-            ]
-        ),
-        height: 60,
-        child: TextField(
-          obscureText: true,
-          style: TextStyle(
-              color: Colors.black87
-          ),
-          decoration: InputDecoration(
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.only(top:14),
-              prefixIcon: Icon(
-                Icons.lock,
-                color: Color(0xff0E0E0E),
-              ),
-              hintText: 'Passowrd',
-              hintStyle: TextStyle(
-                  color: Colors.black38
-              )
-          ),
-        ),
-      )
-    ],
-  );
-}
+    );
+  }
 
-class _LoginPageState extends State<LoginPage>{
   @override
   Widget build (BuildContext context){
     return Scaffold(
@@ -123,10 +230,10 @@ class _LoginPageState extends State<LoginPage>{
                     begin: Alignment.topCenter,
                     end: Alignment.bottomCenter,
                     colors: [
-                      Color(0x6679AAF3),
-                      Color(0x9979AAF3),
-                      Color(0xcc79AAF3),
-                      Color(0xffFFFFFF),
+                      Color(0xB7000000),
+                      Color(0x99616365),
+                      Color(0xccbdb6b6),
+                      Color(0xffffffff),
                     ]
                   )
                 ),
@@ -151,6 +258,11 @@ class _LoginPageState extends State<LoginPage>{
                     buildEmail(),
                     SizedBox(height: 20),
                     buildPassword(),
+                    buildForgotPassBtn(),
+                    buildRememberCb(),
+                    SizedBox(height: 20),
+                    buildLoginBtn(),
+                    buildSignUpBtn()
                   ],
                 ),
                 ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:switch_up/TabPage.dart';
 import 'LoginPage.dart';
 
 void main(){
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget{
       title: "Flutter Login",
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
+
     );
   }
 }
